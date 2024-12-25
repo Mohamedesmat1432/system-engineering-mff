02909f153e17833b129c6b345318c456f7bce9f0
Mohamed esmat <mohamedesmat1432@gmail.com>
2024-12-25 19:09:41 +0200

    last edit

--- app/Console/Commands/DbBackup.php
+++ app/Console/Commands/DbBackup.php
@@ -27,21 +27,6 @@ class DbBackup extends Command
      */
     public function handle()
     {
-        if (! Storage::exists('backup')) {
-            Storage::makeDirectory('backup');
-        }
-
-        // $filenameSql = 'backup-' . Carbon::now()->format('Y-m-d') . '.sql';
-        // $filenameGz = 'backup-' . Carbon::now()->format('Y-m-d') . '.gz';
-        // $commandSql = 'mysqldump --user=' . env('DB_USERNAME') . ' --password=' . env('DB_PASSWORD') . ' --host=' . env('DB_HOST') . ' ' . env('DB_DATABASE') . '  | gzip > ' . storage_path() . '/app/backup/' . $filenameSql;
-        // $commandGz = 'mysqldump --user=' . env('DB_USERNAME') . ' --password=' . env('DB_PASSWORD') . ' --host=' . env('DB_HOST') . ' ' . env('DB_DATABASE') . '  | gzip > ' . storage_path() . '/app/backup/' . $filenameGz;
-        // $returnVar = null;
-        // $output = null;
-        // exec($commandSql, $output, $returnVar);
-        // exec($commandGz, $output, $returnVar);
-        // $this->info('The database backup is successfully');
-
-        // Get database credentials from the .env file
         $dbHost = config('database.connections.mysql.host');
         $dbPort = config('database.connections.mysql.port');
         $dbName = config('database.connections.mysql.database');
@@ -52,9 +37,9 @@ class DbBackup extends Command
         $backupFile = storage_path('app/backup/' . $dbName . '_backup_' . date('Y_m_d_H_i_s') . '.sql');
 
         // Ensure the backups directory exists
-        if (! Storage::exists('backup')) {
-            Storage::makeDirectory('backup');
-        }
+        // if (! Storage::exists('backup')) {
+        //     Storage::makeDirectory('backup');
+        // }
 
         // Build the mysqldump command
         $command = sprintf(
