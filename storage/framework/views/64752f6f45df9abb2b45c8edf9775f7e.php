a22f1d51c9ef78061bd4d05d4bd23fe0c39ea66b
Mohamed esmat <mohamedesmat1432@gmail.com>
2025-11-30 11:21:41 +0200

    last edit from dokki

--- storage/framework/views/64752f6f45df9abb2b45c8edf9775f7e.php
+++
@@ -1,5 +0,0 @@
-<button <?php echo e($attributes->merge(['type' => 'button', 'class' => 'inline-flex items-center px-4 py-2 bg-yellow-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-yellow-700 focus:bg-yellow-500 active:bg-yellow-900 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 transition ease-in-out duration-150'])); ?>>
-    <?php echo e($slot); ?>
-
-</button>
-<?php /**PATH /var/www/resources/views/components/yellow-button.blade.php ENDPATH**/ ?>
